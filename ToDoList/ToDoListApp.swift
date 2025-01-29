84dc263baabebe47fcadc38bb3e8ebd80e28657c
Collin Maillet <cmaillet@lcs.on.ca>
2025-01-28 19:46:56 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Collin Maillet on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
